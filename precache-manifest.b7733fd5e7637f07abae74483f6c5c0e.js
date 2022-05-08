033352ca65d80a57fee74516c0cae7099934f9b5
zhangchun <zhangchun@bonree.com>
2022-05-08 10:33:33 +0800

    fix: test

---
+++ precache-manifest.b7733fd5e7637f07abae74483f6c5c0e.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c9489bbdc6deaa1074049dbf4ce5f6",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/index.html"
+  },
+  {
+    "revision": "46fb75ea50c6362442c7",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/css/3.aff257af.chunk.css"
+  },
+  {
+    "revision": "3ae9e762cb595cdea403",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/css/main.3f5ba7bf.chunk.css"
+  },
+  {
+    "revision": "05c0570d1aca4219112c",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/0.67821d81.chunk.js"
+  },
+  {
+    "revision": "4ae03668a3114932b770",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/10.d0ace796.chunk.js"
+  },
+  {
+    "revision": "0b15ee5c5efbfef4219e",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/11.4ee946d5.chunk.js"
+  },
+  {
+    "revision": "37f83ff1c58fbfc1c47a",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/12.33525db1.chunk.js"
+  },
+  {
+    "revision": "72d235270867608c3d5e",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/13.6a5345a6.chunk.js"
+  },
+  {
+    "revision": "46fb75ea50c6362442c7",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/3.f7c287ba.chunk.js"
+  },
+  {
+    "revision": "d41db0657ecd09feceeb",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/4.b97b791c.chunk.js"
+  },
+  {
+    "revision": "3295d20e9df774fdf151",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/5.c26c8d40.chunk.js"
+  },
+  {
+    "revision": "30f19b7040192828073c",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/6.d96bcb09.chunk.js"
+  },
+  {
+    "revision": "0402e3c2b26f9032930d",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/7.8c21e9ae.chunk.js"
+  },
+  {
+    "revision": "30cce8cd0aa65e7d66d4",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/8.a1a7c9d3.chunk.js"
+  },
+  {
+    "revision": "f241eb4152dde8b2b7d5",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/9.30cd5c7a.chunk.js"
+  },
+  {
+    "revision": "3ae9e762cb595cdea403",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/main.e33be7f2.chunk.js"
+  },
+  {
+    "revision": "cab284bcf89461ba845d",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/js/runtime-main.b48123f1.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https://lazychunstatic.oss-cn-shanghai.aliyuncs.com/web_dist/planet/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ static/js/13.6a5345a6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpplanet-web"]=this["webpackJsonpplanet-web"]||[]).push([[13],{1116:function(n,t,e){"use strict";e.r(t);var a=e(1),r=e(0),i=e.n(r),u=e(2),c=e(450);function o(){var n=Object(a.a)(["\n  margin-top: 40px;\n"]);return o=function(){return n},n}var p=u.a.div(o());t.default=function(n){var t=n.history;return i.a.createElement(p,null,i.a.createElement(c.a,{history:t}))}}}]);
+//# sourceMappingURL=13.6a5345a6.chunk.js.map
